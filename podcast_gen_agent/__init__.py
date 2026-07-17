6b0f8f7261fc40d8b5d7c6cf48aa84fd27f4bd43
Rashal <rashaljeet766@gmail.com>
2026-07-17 15:22:38 +0530

    Update __init__.py

--- podcast_gen_agent/__init__.py
+++ podcast_gen_agent/__init__.py
@@ -1,5 +1,6 @@
 """Podcast-Gen-Agent: Autonomous podcast generator using LangGraph."""
 
-from .compat import ensure_transformers_compat
+from .compat import ensure_langchain_compat, ensure_transformers_compat
 
 ensure_transformers_compat()
+ensure_langchain_compat()
